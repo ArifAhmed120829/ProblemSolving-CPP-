aa01bba581accdfd6965c801df8922891e22638f
ArifAhmed <arif15-12301@diu.edu.bd>
2023-09-16 13:58:56 +0600

    solved
    :)

--- general.cpp
+++ general.cpp
@@ -2,53 +2,42 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
+    int n,count,max,min,max_pos,min_pos;
     cin>>n;
     int arr[n];
     for(int i =0; i<n; i++){
         cin>>arr[i];
     }
-    int min = arr[0];
- int max2 = arr[0];
-    int pos1 = 0;
-    int pos2 = 0;
-    int count  =0;
+     max = arr[0];
+     min = arr[0];
+     max_pos = 0;
+     min_pos = 0;
+     for(int i =1; i<n; i++){
+         if(max<arr[i]){
+            max = arr[i];
+            max_pos = i;
+         }
 
+     }
 
-    for(int i = 0; i+1<n; i++){
+     while(max_pos != 0){
+         swap(arr[max_pos],arr[max_pos-1]);
+          count++;
+        max_pos--;
+     }
+      for(int i =1; i<n; i++){
+          if(min>=arr[i]) {
+            min = arr[i];
+            min_pos = i;
+         }
+     }
+     while(min_pos != (n-1)){
+         swap(arr[min_pos],arr[min_pos+1]);
+         count++;
+        min_pos++;
+     }
+     cout<<count<<endl;
 
-        if(min>=arr[i+1]){
-            min = arr[i+1];
-            pos2 = i+1;
-        }
-            if(max2<=arr[i+1]){
-                max2 = arr[i+1];
-        }
-
-    }
-    if(arr[0] == max2 && arr[n-1] == min) cout<<count<<endl;
-    else{
-         while(pos2+1<n){
-        swap(arr[pos2],arr[pos2+1]);
-        pos2++;
-        count++;
-    }
-
-    int max = arr[0];
-
-      for(int i = 0; i+1<n; i++){
-            if(max<=arr[i+1]){
-                max = arr[i+1];
-                pos1 = i+1;
-        }
-      }
-       while(pos1-1 >= 0){
-        swap(arr[pos1-1],arr[pos1]);
-     pos1--;
-     count++;
-    }
-    cout<<count<<endl;
-    }
 
 
 
