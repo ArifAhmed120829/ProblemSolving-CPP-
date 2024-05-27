cd6c5e5ec73962a28a2def095f348edf8b180166
ArifAhmed <arif15-12301@diu.edu.bd>
2024-05-27 20:01:03 +0600

    added

---
+++ lc6.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define REP(i,a,b) for (int i = a; i < b; i++)
+
+
+int main() {
+string s = "ARIF";
+int n = s[0];
+cout<<n<<"\n";
+char c = char(65+32);
+cout<<c;
+}
